747ff2775ce440642625712f0a1c041d693d70be
AnthonyFerry <a.ferry@ludus-academie.fr>
2016-12-13 16:45:41 +0100

    Ajout menu principal

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public string GameScene;
+
+    public void GoToGame()
+    {
+        if (GameScene == null)
+        {
+            Debug.LogError("The GameScene parameter is not set !");
+            return;
+        }
+
+        SceneManager.LoadScene(GameScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
